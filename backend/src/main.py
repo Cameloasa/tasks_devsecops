18a2e237c1030e13f32e3628d09e16b1da943ded
Camelia Ciuca <159022736+Cameloasa@users.noreply.github.com>
2025-12-10 14:01:48 +0100

    main

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/health", tags=["Health"])
 
+@app.get("/health", tags=["Health"])
 async def health_check():
     return {"status": "ok"}
-
