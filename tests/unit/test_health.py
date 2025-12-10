88d57b54b4676872e769a75e11d0ac8d77f7b659
Camelia Ciuca <159022736+Cameloasa@users.noreply.github.com>
2025-12-10 13:44:49 +0100

    test_health

--- tests/unit/test_health.py
+++ tests/unit/test_health.py
@@ -1,9 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../backend/src"))
+
+from main import app
 from fastapi.testclient import TestClient
-from src.main import app
 
 client = TestClient(app)
 
 def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
